82c37985ba44d955636e91b3b3fc4e3fe00420cc
mby444 <bimayudha161@gmail.com>
2022-06-27 16:08:51 +0700

    Add live search feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 // const formReq = document.querySelector(".form-req");
 const formQuest = document.querySelector(".form-quest");
+const queryInput = document.querySelector("#q-input");
+const suggestionContainer = document.querySelector(".live-suggestion-container");
 
 // const sendUrl = async (url) => {
 //     const payload = JSON.stringify({ url });
@@ -51,6 +53,50 @@ const submitQuestionEvent = async () => {
     clearForm();
 };
 
+const getSuggestions = async (text) => {
+    if (!text) return [];
+    try {
+        const rawResponse = await fetch(`/film/search?q=${text}`);
+        const response = await rawResponse.json();
+        const suggestions = response.results?.map((result) => {
+            let filmId = result.id;
+            let title = result.title;
+            let year = result.release_date?.split("-")[0];
+            return { filmId, title, year };
+        });
+        return suggestions;
+    } catch (err) {
+        console.log(err);
+        return [];
+    }
+}
+
+const getSuggestionElement = (suggestion) => {
+    let year = suggestion.year ? `(${suggestion.year})` : "";
+    let element = `
+        <div class="row p-1 pt-0 pb-0">
+            <a href="/info/${suggestion.filmId}" class="text-dark" style="text-decoration:none;">${suggestion.title} ${year}</a>
+        </div>
+    `;
+    return element;
+};
+
+const displaySuggestions = (suggestions=[]) => {
+    if (suggestions.length === 0) {
+        suggestionContainer.innerHTML = "";
+        suggestionContainer.style.display = "none";
+        return;
+    }
+    suggestionContainer.style.display = "block";
+    let elements = [];
+    suggestions.forEach((suggestion) => {
+        let element = getSuggestionElement(suggestion);
+        elements.push(element);
+    });
+    elements.splice(10, elements.length - 10);
+    suggestionContainer.innerHTML = elements.join("");
+};
+
 // formReq.addEventListener("submit", (event) => {
 //     event.preventDefault();
 //     submitUrlEvent();
@@ -59,4 +105,15 @@ const submitQuestionEvent = async () => {
 formQuest.addEventListener("submit", (event) => {
     event.preventDefault();
     submitQuestionEvent();
+});
+
+queryInput.addEventListener("input", async (event) => {
+    const suggestions = await getSuggestions(event.target.value);
+    displaySuggestions(suggestions);
+});
+
+queryInput.addEventListener("blur", () => {
+    setTimeout(() => {
+        displaySuggestions([]);
+    }, 150);
 });
